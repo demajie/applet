56af6583647c925ec9f720ef5f43f63898c81c00
qwe541099558 <541099558@qq.com>
2020-05-06 17:24:16 +0800

    小区邀请码生成

--- src/main/java/com/applet/controller/AdminController.java
+++ src/main/java/com/applet/controller/AdminController.java
@@ -1,14 +1,20 @@
 package com.applet.controller;
 
 import com.applet.bean.dto.AdminAddInfo;
+import com.applet.bean.vo.AdminDetailInfo;
+import com.applet.bean.vo.AdminSimpleInfo;
 import com.applet.common.JsonWrapper;
 import com.applet.service.AdminService;
+import com.applet.utils.VerificationCodeUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author gsj
@@ -35,11 +41,35 @@ public class AdminController {
         return new JsonWrapper<>(adminService.deleteAdmin(id));
     }
 
-    @ApiOperation("获取小区邀请码")
-    @GetMapping("getCommunityInvitationCode")
-    public JsonWrapper<String> communityInvitationCode(){
-        return null;
+    @ApiOperation("生成小区邀请码")
+    @GetMapping("createInvitationCode")
+    public JsonWrapper<String> createInvitationCode(){
+        return new JsonWrapper<>(VerificationCodeUtils.createInvitationCode());
     }
 
 
+    @ApiOperation("更新管理员工作状态")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id",value = "管理员id",dataType = "Integer"),
+            @ApiImplicitParam(name = "state",value = "新状态,0在岗,1出勤,2节假日")
+    })
+    @GetMapping("updateState/{id}")
+    public JsonWrapper<Boolean> updateState(@PathVariable("id")Integer id,
+             @RequestParam("state")Integer state) {
+            return new JsonWrapper<>(adminService.updateState(id,state));
+    }
+
+    @ApiOperation("得到小区内【所有】管理员简单信息")
+    @ApiImplicitParam(name = "communityId",value = "社区id",dataType = "Integer")
+    @GetMapping("getAdminSimpInfos")
+    public JsonWrapper<List<AdminSimpleInfo>> getAdminSimpInfos(Integer communityId){
+        return new JsonWrapper<>(adminService.getAdminSimpInfos(communityId));
+    }
+
+    @ApiOperation("得到【一个】管理员的详细信息")
+    @ApiImplicitParam(name = "id",value = "管理员id",dataType = "Integer")
+    @GetMapping("getAdminDetailInfo")
+    public JsonWrapper<AdminDetailInfo> getAdminDetailInfo(Integer id){
+        return new JsonWrapper<>(adminService.getAdminDetailInfo(id));
+    }
 }
--- src/main/java/com/applet/utils/VerificationCodeUtils.java
+++ src/main/java/com/applet/utils/VerificationCodeUtils.java
@@ -11,17 +11,15 @@ public class VerificationCodeUtils {
 
     /**
      *  生成6位小区邀请码,如果已经存在,则重新生成
+     *  待完善:检验是否存在
      */
     public static String createInvitationCode() {
         Random random = new Random();
-        String invitationCode = "";
+        StringBuilder builder = new StringBuilder();
         for (int i = 0; i < 6; i++) {
-            invitationCode += random.nextInt(10);
+            builder.append(random.nextInt(10));
         }
-        System.out.println(invitationCode);
-        return invitationCode;
+        return builder.toString();
     }
-
-
 
 }
